589d36c9f9d4173246b3707dec5686e18bdd2c19
madcat <kr.madcat@gmail.com>
2015-06-02 09:21:16 +0900

    test yuidocs

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -9,8 +9,15 @@ module.exports = {
     port : 6379,
     db : 2,
 
-    //-------------------------------
-    // Redis 서버가 응답이 없을시 에러 처리.
+
+    /**
+     * Redis 서버가 응답이 없을시 에러 처리.
+     *
+     * @method errorHandler
+     * @param req
+     * @param res
+     * @param next
+     */
     errorHandler: function(req, res, next) {
         if( "undefined" === typeof req.session) {
             throw new Error(' Lost Redis Server Connection. ');
